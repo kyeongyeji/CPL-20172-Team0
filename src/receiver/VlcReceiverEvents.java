28e9344fd1fa11a4f0d0414112494f34fbbf52b1
CHEOLMIN KIM <cheolminkim@vanilet.pe.kr>
2017-10-29 21:13:04 +0900

    receivedSuccessfully() 오타 수정

--- src/receiver/VlcReceiverEvents.java
+++ src/receiver/VlcReceiverEvents.java
@@ -6,5 +6,5 @@ public interface VlcReceiverEvents extends EventListener {
 	public void receiverSelected();
 	public void receiverHasError();
 	public void receiverHasMessage();
-	public void recivedSuccessfully();
+	public void receivedSuccessfully();
 }
