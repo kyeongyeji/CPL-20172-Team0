d9b6354b0fd4340632d8e8d0234caceaec60c5a4
KyeongYeJi <kyg9353@knu.ac.kr>
2017-10-30 22:59:18 +0900

    Commit test...

--- src/receiver/VlcReceiverEvents.java
+++ src/receiver/VlcReceiverEvents.java
@@ -1,10 +1,11 @@
 package receiver;
 
 import java.util.EventListener;
+import jssc.SerialPortException;
 
 public interface VlcReceiverEvents extends EventListener {
 	public void receiverSelected();
-	public void receiverHasError();
+	public void receiverHasError(SerialPortException ex);
 	public void receiverHasMessage();
 	public void receivedSuccessfully();
 }
